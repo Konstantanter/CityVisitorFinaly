77ed847789511ab0220e7c3e17c396c3808cde58
onufriev97 <onufriev97@mail.ru>
2024-10-18 10:13:25 +0300

    Инициализирована Курская область

--- CityVisitorFinaly/AppData/InitialCity.cs
+++ CityVisitorFinaly/AppData/InitialCity.cs
@@ -57,6 +57,8 @@ namespace CityVisitorFinaly
             KalugaOblast();
             //Костромская область
             KostromskayaObl();
+            //Курская область
+            Kurskoblast();
             //regionNames.Add("path72", "Краснодарский край");
             //regionNames.Add("path74", "Красноярский край");
             //regionNames.Add("path44", "Республика Саха (Якутия)");
---
+++ CityVisitorFinaly/AppData/Regions/Kurskoblast.cs
@@ -0,0 +1,42 @@
+﻿using CityVisitorFinaly.AppData;
+
+namespace CityVisitorFinaly
+{
+    public partial class RegionsPage : ContentPage
+    {
+        /// <summary>
+        /// Инициализация Курской области области
+        /// </summary>
+        public void Kurskoblast()
+        {
+            Regions Kurskoblast = new Regions("Курская область", "kurskoblast.png")
+            {
+                IdRegionsMaps = "path120"
+            };
+
+            City dmitrev = new City("Дмитриев", "alupka.png");
+            City zelezno = new City("Железногорск", "alupka.png");
+            City kursk = new City("Курск", "kursk.png");
+            City kurchatov = new City("Курчатов", "kurchatov.png");
+            City lgov = new City("Льгов", "lgov.png");
+            City oboyan = new City("Обоянь", "oboyan.png");
+            City rylsk = new City("Рыльск", "rylsk.png");
+            City sudza = new City("Суджа", "sudza.png");
+            City fatezh = new City("Фатеж", "fatezh.png");
+            City schigry = new City("Щигры", "schigry.png");
+
+            Kurskoblast.AddCities(dmitrev);
+            Kurskoblast.AddCities(zelezno);
+            Kurskoblast.AddCities(kursk);
+            Kurskoblast.AddCities(kurchatov);
+            Kurskoblast.AddCities(lgov);
+            Kurskoblast.AddCities(oboyan);
+            Kurskoblast.AddCities(rylsk);
+            Kurskoblast.AddCities(sudza);
+            Kurskoblast.AddCities(fatezh);
+            Kurskoblast.AddCities(schigry);
+
+            AddRegToDB(Kurskoblast);
+        }
+    }
+}
