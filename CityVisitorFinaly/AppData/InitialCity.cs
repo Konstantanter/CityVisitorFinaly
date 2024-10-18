4edb666baea13c33ff67325963d2fbab0120ddee
onufriev97 <onufriev97@mail.ru>
2024-10-18 09:30:35 +0300

    Инициализирована Костромская область

--- CityVisitorFinaly/AppData/InitialCity.cs
+++ CityVisitorFinaly/AppData/InitialCity.cs
@@ -55,6 +55,8 @@ namespace CityVisitorFinaly
             IvanovoOblast();
             //Калужская область
             KalugaOblast();
+            //Костромская область
+            KostromskayaObl();
             //regionNames.Add("path72", "Краснодарский край");
             //regionNames.Add("path74", "Красноярский край");
             //regionNames.Add("path44", "Республика Саха (Якутия)");
---
+++ CityVisitorFinaly/AppData/Regions/KostromskayaObl.cs
@@ -0,0 +1,43 @@
+﻿using CityVisitorFinaly.AppData;
+
+namespace CityVisitorFinaly
+{
+    public partial class RegionsPage : ContentPage
+    {
+        /// <summary>
+        /// Инициализация Костромской области
+        /// </summary>
+        public void KostromskayaObl()
+        {
+            Regions KostromskayaObl = new Regions("Костромская область", "kostromskayaobl.png")
+            {
+                IdRegionsMaps = "path116"
+            };
+            City bui = new City("Буй", "bui.png");
+            City volgorechensk = new City("Волгореченск", "volgorechensk.png");
+            City galich = new City("Галич", "galich.png");
+            City kologriv = new City("Кологрив", "kologriv.png");
+            City kostroma = new City("Кострома", "kostroma.png");
+            City makarjev = new City("Макарьев", "makarjev.png");
+            City nerehta = new City("Нерехта", "nerehta.png");
+            City neya = new City("Нея", "neya.png");
+            City soligalich = new City("Солигалич", "soligalich.png");
+            City chuhloma = new City("Чухлома", "chuhloma.png");
+            City sharya = new City("Шарья", "sharya.png");
+
+            KostromskayaObl.AddCities(bui);
+            KostromskayaObl.AddCities(volgorechensk);
+            KostromskayaObl.AddCities(galich);
+            KostromskayaObl.AddCities(kologriv);
+            KostromskayaObl.AddCities(kostroma);
+            KostromskayaObl.AddCities(makarjev);
+            KostromskayaObl.AddCities(nerehta);
+            KostromskayaObl.AddCities(neya);
+            KostromskayaObl.AddCities(soligalich);
+            KostromskayaObl.AddCities(chuhloma);
+            KostromskayaObl.AddCities(sharya);
+
+            AddRegToDB(KostromskayaObl);
+        }
+    }
+}
