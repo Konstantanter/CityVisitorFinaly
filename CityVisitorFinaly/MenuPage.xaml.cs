ce0b2fe4d491424c79856a909158769b6ab445d4
onufriev97 <onufriev97@mail.ru>
2024-10-28 14:13:06 +0300

    Теперь меню откоывается и работает, есть желание сделать его немного приятнее, но это немного позже

--- CityVisitorFinaly/MenuPage.xaml.cs
+++ CityVisitorFinaly/MenuPage.xaml.cs
@@ -5,20 +5,16 @@ public partial class MenuPage : ContentPage
     Page MainPage;
 	public MenuPage(Page page)
 	{
-     
-
         InitializeComponent();
 	}
-    private void ButtonMaps_Clicked(object sender, EventArgs e)
+    private async void ButtonMaps_Clicked(object sender, EventArgs e)
     {
-        App app = new App();
-        app.ShowForm(new NavigationPage(new MapsPage()));
+        await Navigation.PushAsync(new MapsPage());
+
    
     }
-    private void ButtonRegion_Clicked(object sender, EventArgs e)
+    private async void ButtonRegion_Clicked(object sender, EventArgs e)
     {
-        App app = new App();
-        app.ShowForm(new NavigationPage(new RegionsPage()));
-        //DisplayAlert("Информационное сообщение", "Ты кликнул на Регионы", "Ок");
+        await Navigation.PushAsync(new RegionsPage());
     }
 }
