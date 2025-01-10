8f169e57aade4550f793446c5c7ae10063c90dc8
onufriev97 <onufriev97@mail.ru>
2025-01-10 09:25:23 +0300

    Инициализирована Ярославская область и ЦФО в целом

--- CityVisitorFinaly/AppData/InitialCity.cs
+++ CityVisitorFinaly/AppData/InitialCity.cs
@@ -72,6 +72,8 @@ namespace CityVisitorFinaly
             TverskayaObl();
             //Тульская область
             TulskayaObl();
+            //Ярославская область
+            YaroslavskayaObl();
             //regionNames.Add("path72", "Краснодарский край");
             //regionNames.Add("path74", "Красноярский край");
             //regionNames.Add("path44", "Республика Саха (Якутия)");
