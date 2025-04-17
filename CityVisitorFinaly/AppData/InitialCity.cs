cb8fe63522c1afa56fe6537b11d5f2f85f11b28a
Konstantanter <onufriev97@mail.ru>
2025-04-17 11:28:52 +0300

    Инициализирован ЯНАО

--- CityVisitorFinaly/AppData/InitialCity.cs
+++ CityVisitorFinaly/AppData/InitialCity.cs
@@ -84,6 +84,8 @@ namespace CityVisitorFinaly
             KhantymansiyskiyAO();
             //Челябинская область
             ChelyabinskayaObl();
+            //Ямало-Ненецкий АО
+            YamalnenetsiaAO();
             //regionNames.Add("path72", "Краснодарский край");
             //regionNames.Add("path74", "Красноярский край");
             //regionNames.Add("path44", "Республика Саха (Якутия)");
---
+++ CityVisitorFinaly/AppData/Regions/YamalnenetsiaAO.cs
@@ -0,0 +1,28 @@
+﻿using CityVisitorFinaly.AppData;
+
+namespace CityVisitorFinaly
+{
+    public partial class RegionsPage : ContentPage
+    {
+        /// <summary>
+        /// Инициализация Ярославской области
+        /// </summary>
+        public void YamalnenetsiaAO()
+        {
+            Regions YamalnenetsiaAO = new Regions("Ямало Ненецкий АО", "yamalnenetsia.png")
+            {
+                IdRegionsMaps = "path361"
+            };
+
+            YamalnenetsiaAO.AddCities(new City("Губкинский", "gubkinsky.png"));
+            YamalnenetsiaAO.AddCities(new City("Лабытнанги", "labytnangi.png"));
+            YamalnenetsiaAO.AddCities(new City("Муравленко", "muravlenko.png"));
+            YamalnenetsiaAO.AddCities(new City("Надым", "nadym.png"));
+            YamalnenetsiaAO.AddCities(new City("Новый Уренгой", "novyurengoy.png"));
+            YamalnenetsiaAO.AddCities(new City("Ноябрьск", "noyabrsk.png"));
+            YamalnenetsiaAO.AddCities(new City("Салехард", "salekhard.png"));
+            YamalnenetsiaAO.AddCities(new City("Тарко-Сале", "tarkosale.png"));
+            AddRegToDB(YamalnenetsiaAO);
+        }
+    }
+}
