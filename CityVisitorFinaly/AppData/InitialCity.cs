ec9830c874e114855bc3d861ee9387dbb464cb67
onufriev97 <onufriev97@mail.ru>
2025-10-22 14:21:24 +0300

    Инициализирована Калаининградская область

--- CityVisitorFinaly/AppData/InitialCity.cs
+++ CityVisitorFinaly/AppData/InitialCity.cs
@@ -112,6 +112,8 @@ namespace CityVisitorFinaly
             ArkhangelskObl();
             //Вологодская область
             VologdaOblast();
+            //Калининградская область
+            KaliningradObl();
             //regionNames.Add("path72", "Краснодарский край");
             //regionNames.Add("path74", "Красноярский край");
             //regionNames.Add("path44", "Республика Саха (Якутия)");
---
+++ CityVisitorFinaly/AppData/Regions/KaliningradObl.cs
@@ -0,0 +1,43 @@
+﻿using CityVisitorFinaly.AppData;
+
+namespace CityVisitorFinaly
+{
+    public partial class RegionsPage : ContentPage
+    {
+        /// <summary>
+        /// Инициализация Калининградской  области
+        /// </summary>
+        public void KaliningradObl()
+        {
+            Regions KaliningradObl = new Regions("Калининградская область", "kaliningradobl.png")
+            {
+                IdRegionsMaps = "path108"
+            };
+            KaliningradObl.AddCities(new City("Багратионовск", "bagrationovsk.png"));
+            KaliningradObl.AddCities(new City("Балтийск", "baltiysk.png"));
+            KaliningradObl.AddCities(new City("Гвардейск", "gvardeisk.png"));
+            KaliningradObl.AddCities(new City("Гурьевск", "guryevskkaliningrad.png"));
+            KaliningradObl.AddCities(new City("Гусев", "gusiew.png"));
+            KaliningradObl.AddCities(new City("Зеленоградск", "zelenogradsk.png"));
+            KaliningradObl.AddCities(new City("Калининград", "kaliningrad.png"));
+            KaliningradObl.AddCities(new City("Краснознаменск", "krasnoznamenskkaliningrad.png"));
+            KaliningradObl.AddCities(new City("Ладушкин", "ladushkin.png"));
+            KaliningradObl.AddCities(new City("Мамоново", "mamonovo.png"));
+            KaliningradObl.AddCities(new City("Неман", "neman.png"));
+            KaliningradObl.AddCities(new City("Нестеров", "nesterov.png"));
+            KaliningradObl.AddCities(new City("Озёрск", "ozersk.png"));
+            KaliningradObl.AddCities(new City("Пионерский", "pionersky.png"));
+            KaliningradObl.AddCities(new City("Полесск", "polessk.png"));
+            KaliningradObl.AddCities(new City("Правдинск", "pravdinsk.png"));
+            KaliningradObl.AddCities(new City("Приморск", "primorsk.png"));
+            KaliningradObl.AddCities(new City("Светлогорск", "svetlogorsk.png"));
+            KaliningradObl.AddCities(new City("Светлый", "svetly.png"));
+            KaliningradObl.AddCities(new City("Славск", "slavsk.png"));
+            KaliningradObl.AddCities(new City("Советск", "sovetskkaliningrad.png"));
+            KaliningradObl.AddCities(new City("Черняховск", "chernyakhovsk.png"));
+
+            KaliningradObl.Append_Reg = AppendReg.RegAppend.ToString();
+            AddRegToDB(KaliningradObl);
+        }
+    }
+}
