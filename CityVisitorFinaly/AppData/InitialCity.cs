20f50fc0458d53972156e19c49fb9ad3c53249fe
onufriev97 <onufriev97@mail.ru>
2025-10-31 21:52:43 +0300

    Инициализирован Ненецкий АО

--- CityVisitorFinaly/AppData/InitialCity.cs
+++ CityVisitorFinaly/AppData/InitialCity.cs
@@ -118,7 +118,8 @@ namespace CityVisitorFinaly
             LeningradObl();
             //Мурманская область
             MurmanskayaObl();
-
+            //Ненецкий АО
+            NenetsAO();
             //regionNames.Add("path72", "Краснодарский край");
             //regionNames.Add("path74", "Красноярский край");
             //regionNames.Add("path44", "Республика Саха (Якутия)");
---
+++ CityVisitorFinaly/AppData/Regions/NenetsAO.cs
@@ -0,0 +1,21 @@
+﻿using CityVisitorFinaly.AppData;
+
+namespace CityVisitorFinaly
+{
+    public partial class RegionsPage : ContentPage
+    {
+        /// <summary>
+        /// Инициализация Ненецкого АО
+        /// </summary>
+        public void NenetsAO()
+        {
+            Regions NenetsAO = new Regions("Ненецкий АО", "nenetsautonomousokrug.png")
+            {
+                IdRegionsMaps = "path355"
+            };
+            NenetsAO.AddCities(new City("Нарьян-Мар", "naryanmar.png"));
+            NenetsAO.Append_Reg = AppendReg.RegAppend.ToString();
+            AddRegToDB(NenetsAO);
+        }
+    }
+}
