3872fe4d4897d015ba056f92abc62888f720e988
onufriev97 <onufriev97@mail.ru>
2026-01-05 16:41:15 +0300

    Инициализирована Пензенская область

--- CityVisitorFinaly/AppData/InitialCity.cs
+++ CityVisitorFinaly/AppData/InitialCity.cs
@@ -138,6 +138,8 @@ namespace CityVisitorFinaly
             NizhnyNovgorodObl();
             //Оренбургская область
             OrenburgObl();
+            //Пензенская область
+            PenzaObl();
             //regionNames.Add("path72", "Краснодарский край");
             //regionNames.Add("path74", "Красноярский край");
             //regionNames.Add("path44", "Республика Саха (Якутия)");
---
+++ CityVisitorFinaly/AppData/Regions/PenzaObl.cs
@@ -0,0 +1,32 @@
+﻿using CityVisitorFinaly.AppData;
+
+namespace CityVisitorFinaly
+{
+    public partial class RegionsPage : ContentPage
+    {
+        /// <summary>
+        /// Инициализация Пензенской области
+        /// </summary>
+        public void PenzaObl()
+        {
+            Regions PenzaObl = new Regions("Пензенская область", "penzaobl.png")
+            {
+                IdRegionsMaps = "path144"
+            };
+            PenzaObl.AddCities(new City("Белинский", "belinsky.png"));
+            PenzaObl.AddCities(new City("Городище", "buguruslan.png"));
+            PenzaObl.AddCities(new City("Заречный", "zarechny.png"));
+            PenzaObl.AddCities(new City("Каменка", "kamenka.png"));
+            PenzaObl.AddCities(new City("Кузнецк", "kuznetsk.png"));
+            PenzaObl.AddCities(new City("Нижний Ломов", "nizhnylomov.png"));
+            PenzaObl.AddCities(new City("Никольск", "nikolskpenzaobl.png"));
+            PenzaObl.AddCities(new City("Пенза", "penza.png"));
+            PenzaObl.AddCities(new City("Сердобск", "serdobsk.png"));
+            PenzaObl.AddCities(new City("Спасск", "spassk.png"));
+            PenzaObl.AddCities(new City("Сурск", "sursk.png"));
+
+            PenzaObl.Append_Reg = AppendReg.RegAppend.ToString();
+            AddRegToDB(PenzaObl);
+        }
+    }
+}
