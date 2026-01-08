b6d425ecf6b4fd2d5b557f60486e67f5d4eac74d
onufriev97 <onufriev97@mail.ru>
2026-01-08 19:01:10 +0300

    Инициализирован Татарстан и внесены небольшие правки в Самарскую область

---
+++ CityVisitorFinaly/AppData/Regions/RespublicTatarstan.cs
@@ -0,0 +1,45 @@
+﻿using CityVisitorFinaly.AppData;
+
+namespace CityVisitorFinaly
+{
+    public partial class RegionsPage : ContentPage
+    {
+        /// <summary>
+        /// Инициализация Республики Татарстан
+        /// </summary>
+        public void RespublicTatarstan()
+        {
+            Regions RespublicTatarstan = new Regions("Республика Татарстан", "tatarstan.png")
+            {
+                IdRegionsMaps = "path48"
+            };
+            RespublicTatarstan.AddCities(new City("Агрыз", "agryz.png"));
+            RespublicTatarstan.AddCities(new City("Азнакаево", "aznakevo.png"));
+            RespublicTatarstan.AddCities(new City("Альметьевск", "almetievsk.png"));
+            RespublicTatarstan.AddCities(new City("Арск", "arsk.png"));
+            RespublicTatarstan.AddCities(new City("Бавлы", "bavly.png"));
+            RespublicTatarstan.AddCities(new City("Болгар", "bolgar.png"));
+            RespublicTatarstan.AddCities(new City("Бугульма", "bugulma.png"));
+            RespublicTatarstan.AddCities(new City("Буинск", "samara.png"));
+            RespublicTatarstan.AddCities(new City("Елабуга", "syzran.png"));
+            RespublicTatarstan.AddCities(new City("Заинск", "zainsk.png"));
+            RespublicTatarstan.AddCities(new City("Зеленодольск", "zelenodolsk.png"));
+            RespublicTatarstan.AddCities(new City("Иннополис", "otradny.png"));
+            RespublicTatarstan.AddCities(new City("Казань", "kazan.png"));
+            RespublicTatarstan.AddCities(new City("Кукмор", "kukmor.png"));
+            RespublicTatarstan.AddCities(new City("Лаишево", "laishevo.png"));
+            RespublicTatarstan.AddCities(new City("Лениногорск", "leninogorsk.png"));
+            RespublicTatarstan.AddCities(new City("Мамадыш", "mamadysh.png"));
+            RespublicTatarstan.AddCities(new City("Менделеевск", "mendeleevsk.png"));
+            RespublicTatarstan.AddCities(new City("Мензелинск", "menzelinsk.png"));
+            RespublicTatarstan.AddCities(new City("Набережные Челны", "naberezhnyechelny.png"));
+            RespublicTatarstan.AddCities(new City("Нижнекамск", "nizhnekamsk.png"));
+            RespublicTatarstan.AddCities(new City("Нурлат", "nurlat.png"));
+            RespublicTatarstan.AddCities(new City("Тетюши", "tetiushi.png"));
+            RespublicTatarstan.AddCities(new City("Чистополь", "chapaevsk.png"));
+
+            RespublicTatarstan.Append_Reg = AppendReg.RegAppend.ToString();
+            AddRegToDB(RespublicTatarstan);
+        }
+    }
+}
--- CityVisitorFinaly/AppData/Regions/SamaraObl.cs
+++ CityVisitorFinaly/AppData/Regions/SamaraObl.cs
@@ -9,21 +9,21 @@ namespace CityVisitorFinaly
         /// </summary>
         public void SamaraObl()
         {
-            Regions SamaraObl = new Regions("Самарская область", "SamaraObl.png")
+            Regions SamaraObl = new Regions("Самарская область", "samaraobl.png")
             {
                 IdRegionsMaps = "path152"
             };
-            SamaraObl.AddCities(new City("Жигулёвск", "arkadak.png"));
-            SamaraObl.AddCities(new City("Кинель", "atkarsk.png"));
-            SamaraObl.AddCities(new City("Нефтегорск", "balakovo.png"));
-            SamaraObl.AddCities(new City("Новокуйбышевск", "balashov.png"));
-            SamaraObl.AddCities(new City("Октяябрьск", "volsk.png"));
-            SamaraObl.AddCities(new City("Отрадный", "ershov.png"));
-            SamaraObl.AddCities(new City("Похвистнево", "kalininsk.png"));
-            SamaraObl.AddCities(new City("Самара", "krasnoarmeysksaratov.png"));
-            SamaraObl.AddCities(new City("Сызрань", "krasnyikyt.png"));
-            SamaraObl.AddCities(new City("Тольятти", "marks.png"));
-            SamaraObl.AddCities(new City("Чапаевск", "novouzensk.png"));
+            SamaraObl.AddCities(new City("Жигулёвск", "zhigulyovsk.png"));
+            SamaraObl.AddCities(new City("Кинель", "kinel.png"));
+            SamaraObl.AddCities(new City("Нефтегорск", "neftegorsk.png"));
+            SamaraObl.AddCities(new City("Новокуйбышевск", "novokuybyshevsk.png"));
+            SamaraObl.AddCities(new City("Октябрьск", "oktyabrsk.png"));
+            SamaraObl.AddCities(new City("Отрадный", "otradny.png"));
+            SamaraObl.AddCities(new City("Похвистнево", "pokhvistnevo.png"));
+            SamaraObl.AddCities(new City("Самара", "samara.png"));
+            SamaraObl.AddCities(new City("Сызрань", "syzran.png"));
+            SamaraObl.AddCities(new City("Тольятти", "togliattil.png"));
+            SamaraObl.AddCities(new City("Чапаевск", "chapaevsk.png"));
 
             SamaraObl.Append_Reg = AppendReg.RegAppend.ToString();
             AddRegToDB(SamaraObl);
