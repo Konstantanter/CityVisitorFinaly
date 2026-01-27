5924a46b5e5ac02c0d69528dbf7a0ccffb150f05
onufriev97 <onufriev97@mail.ru>
2026-01-27 15:32:36 +0300

    Инициализирована Еврейская АО

--- CityVisitorFinaly/AppData/InitialCity.cs
+++ CityVisitorFinaly/AppData/InitialCity.cs
@@ -174,6 +174,8 @@ namespace CityVisitorFinaly
             MagadanskayaObl();
             //Камчатский край
             KamchatckiyKrai();
+            //Еврейская АО
+            EvreiskayaAO();
             //regionNames.Add("path72", "Краснодарский край");
             //regionNames.Add("path74", "Красноярский край");
             //regionNames.Add("path44", "Республика Саха (Якутия)");
---
+++ CityVisitorFinaly/AppData/Regions/EvreiskayaAO.cs
@@ -0,0 +1,24 @@
+﻿using CityVisitorFinaly.AppData;
+
+namespace CityVisitorFinaly
+{
+    public partial class RegionsPage : ContentPage
+    {
+        /// <summary>
+        /// Инициализация Приморского края
+        /// </summary>
+        public void EvreiskayaAO()
+        {
+            Regions EvreiskayaAO = new Regions("Еврейская АО", "jewishautonomousobl.png")
+            {
+                IdRegionsMaps = "path353"
+            };
+
+            EvreiskayaAO.AddCities(new City("Биробиджан", "birobidzhan.png"));
+            EvreiskayaAO.AddCities(new City("Облучье", "obluche.png"));
+
+            EvreiskayaAO.Append_Reg = AppendReg.RegAppend.ToString();
+            AddRegToDB(EvreiskayaAO);
+        }
+    }
+}
